cfbf0ed4dfbbc3322cae62a66edd579d759e893f
BertFrontEnd <Berton@yandex.ru>
2022-08-01 20:01:44 +0300

    chore: remove the track

--- assets/modules/dockets.js
+++ assets/modules/dockets.js
@@ -1,15 +1,25 @@
 const dir = './assets/music/';
-const playList = ['Cartoon-On-&-ON', 'Elektronomia', 'Johnning', 'Popsicle', 'Fearless'];
-const title = ['Cartoon - On & On', 'Elektronomia', 'Janji-Heroes Tonight', 'Popsicle', 'Lost Sky-Fearless'];
+const playList = ['Elektronomia', 'Johnning', 'Popsicle', 'Fearless'];
+const title = [
+  'Elektronomia',
+  'Janji-Heroes Tonight',
+  'Popsicle',
+  'Lost Sky-Fearless',
+];
 
 const artists = [
-  '(feat. Daniel Levi) [NCS Release]',
   'Elektronomia-Sky High [NCS Release]',
   '(feat. Johnning) [NCS Release]',
   'LFZ - [NCS Release]',
   '(feat. Chris Linton) [NCS Release]',
 ];
 
-const poster = ['./assets/img/ncs1.jpeg', './assets/img/ncs2.jpg', './assets/img/ncs3.jpg', './assets/img/ncs4.jpg', './assets/img/ncs5.jpg'];
+const poster = [
+  './assets/img/ncs1.jpeg',
+  './assets/img/ncs2.jpg',
+  './assets/img/ncs3.jpg',
+  './assets/img/ncs4.jpg',
+  './assets/img/ncs5.jpg',
+];
 
 export { dir, playList, title, artists, poster };
